53c83093174447a48be447ffca6b9d9c02d0c5f8
Duane Rush <desertduane@yahoo.com>
2017-07-25 11:26:56 -0700

    refactored

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ function Image(number) {
   this.timesClicked = 0;
   Image.all.push(this);
 }
-
+Image.totalClicks = 0;
 Image.all = [];
 Image.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+var previouslyShown = [];
 
 for(var i = 0; i < Image.allNames.length; i++) {
   new Image(Image.allNames[i]);
@@ -22,59 +24,124 @@ for(var i = 0; i < Image.allNames.length; i++) {
 Image.imgEl1 = document.getElementById('image1');
 Image.imgEl2 = document.getElementById('image2');
 Image.imgEl3 = document.getElementById('image3');
+Image.container = document.getElementById('products');
+
+function makeRandomNumber(){
+  return Math.floor(Math.random() * Image.all.length)
+}
 
 
+function displayImages() {
+  console.log(previouslyShown, 'previously shown images');
+  var numbers = [];
 
+  numbers[0] = makeRandomNumber();
+  numbers[1] = makeRandomNumber();
 
-var randomImage = function () {
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  Image.imgEl1.src = Image.all[randomIndex].source;
-  Image.imgEl1.alt = Image.all[randomIndex].name;
-  duplicatePreventer();
-  Image.all[randomIndex].timesShown += 1;
+  while(numbers[0] === numbers[1]){
+    console.log('doup found');
+    numbers[1] = makeRandomNumber();
+  }
+  numbers[2] = makeRandomNumber();
+  while(numbers[2] === numbers[1] || numbers[2] === numbers[0]){
+    console.log('doup found');
+    numbers[2] = makeRandomNumber();
 
-  console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
+  }
+  Image.imgEl1.src = Image.all[numbers[0]].source;
+  Image.imgEl2.src = Image.all[numbers[1]].source;
+  Image.imgEl3.src = Image.all[numbers[2]].source;
+  Image.all[numbers[0]].timesShown += 1;
+  Image.all[numbers[1]].timesShown += 1;
+  Image.all[numbers[2]].timesShown += 1;
+  console.log(numbers, 'currently showing');
+  previouslyShown = numbers;
+}
 
-};
+function showList(){
+  var ulEl = document.getElementById('thelist');
+  for(var i = 0; i < Image.all.length; i++){
 
-var randomImage2 = function () {
-
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  Image.imgEl2.src = Image.all[randomIndex].source;
-  Image.imgEl2.alt = Image.all[randomIndex].name;
-  duplicatePreventer();
-  Image.all[randomIndex].timesShown += 1;
-
-  console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
-
-};
-var randomImage3 = function () {
-
-  var randomIndex = Math.floor(Math.random() * Image.all.length);
-  Image.imgEl3.src = Image.all[randomIndex].source;
-  Image.imgEl3.alt = Image.all[randomIndex].name;
-  duplicatePreventer();
-  Image.all[randomIndex].timesShown += 1;
-
-  console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
-
-};
-function duplicatePreventer() {
-  if(Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src){
-    randomImage();
-    console.log('re-rolled');
-  }else if(Image.imgEl2.src === Image.imgEl3.src){
-    randomImage2();
-    console.log('re-rolled');
+    var liEl = document.createElement('li');
+    liEl.textContent = Image.all[i].name + ' was shown ' + Image.all[i].timesShown + ' times and was clicked ' + Image.all[i].timesClicked + ' times.';
+    ulEl.appendChild(liEl);
   }
 }
-function getRandomImages() {
-  randomImage();
-  randomImage2();
-  randomImage3();
-}
 
-document.getElementById('products').addEventListener('click', getRandomImages);
+function handleClick(e) {
+  Image.totalClicks += 1;
+
+  console.log(Image.totalClicks, 'total clicks');
+
+  for(var i = 0; i < Image.all.length; i++){
+    if(e.target.alt === Image.all[i].name){
+      Image.all[i].timesClicked += 1;
+    }
+  }
+  if(Image.totalClicks === 3) {
+    Image.container.removeEventListener('click', handleClick);
+
+    showList();
+  }
+
+
+  displayImages();
+}
+displayImages();
+
+Image.container.addEventListener('click', handleClick)
+
+
+
+// var randomImage = function () {
+//   var randomIndex = Math.floor(Math.random() * Image.all.length);
+//   Image.imgEl1.src = Image.all[randomIndex].source;
+//   Image.imgEl1.alt = Image.all[randomIndex].name;
+//   duplicatePreventer();
+//   Image.all[randomIndex].timesShown += 1;
+//
+//   console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
+//
+// };
+//
+// var randomImage2 = function () {
+//
+//   var randomIndex = Math.floor(Math.random() * Image.all.length);
+//   Image.imgEl2.src = Image.all[randomIndex].source;
+//   Image.imgEl2.alt = Image.all[randomIndex].name;
+//   duplicatePreventer();
+//   Image.all[randomIndex].timesShown += 1;
+//
+//   console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
+//
+// };
+// var randomImage3 = function () {
+//
+//   var randomIndex = Math.floor(Math.random() * Image.all.length);
+//   Image.imgEl3.src = Image.all[randomIndex].source;
+//   Image.imgEl3.alt = Image.all[randomIndex].name;
+//   duplicatePreventer();
+//   Image.all[randomIndex].timesShown += 1;
+//
+//   console.log(Image.all[randomIndex].name + 'has been shown ' + Image.all[randomIndex].timesShown + ' times');
+//
+// };
+// function duplicatePreventer() {
+//   if(Image.imgEl1.src === Image.imgEl2.src || Image.imgEl1.src === Image.imgEl3.src){
+//     randomImage();
+//     console.log('re-rolled');
+//   }else if(Image.imgEl2.src === Image.imgEl3.src){
+//     randomImage2();
+//     console.log('re-rolled');
+//   }
+// }
+// function getRandomImages() {
+//   randomImage();
+//   randomImage2();
+//   randomImage3();
+// }
+//
+// document.getElementById('products').addEventListener('click', getRandomImages);
 // document.getElementById('products').addEventListener('click', randomImage2);
 // document.getElementById('products').addEventListener('click', randomImage3);
 
